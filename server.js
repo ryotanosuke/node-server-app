9a51e0554407cf043de9ec2cc1d32f38b2c53b38
ryotanosuke <sub_player-github@yahoo.co.jp>
2022-11-05 21:43:13 +0900

    OK

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ require("dotenv").config();
 const PORT = 5000;
 const cors = require("cors");
 
-// app.use(
-//   cors({
-//     // 他サーバーのエンドポイントを許可
-//     // 語尾にスラッシュいらない
-//     origin: "https://react-front-app-six.vercel.app",
-//   })
-// );
+app.use(
+  cors({
+    // 他サーバーのエンドポイントを許可
+    // 語尾にスラッシュいらない
+    origin: "https://react-front-app-six.vercel.app",
+  })
+);
 
 app.use(
   cors({
